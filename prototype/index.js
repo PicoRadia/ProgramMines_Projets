b9d5073ecf392abf42c0169be8998f57b26a4156
Diettrich <anouar-caesar@outlook.fr>
2019-11-25 21:43:41 +0100

    upgrade anim

--- prototype/index.js
+++ prototype/index.js
@@ -11,12 +11,14 @@ function showMore() {
         textContener.style.height = '164px';
         fog.style.transition = 'background 1s ease';
         fog.style.background = 'linear-gradient(rgba(255, 255, 255, 0), rgba(255, 255, 255, 1))';
+        this.innerHTML = 'afficher plus';
 
         textContener.affiche = 'false';
     } else {    
         textContener.style.height = text.clientHeight + 'px';
         fog.style.transition = 'background 1s ease';
         fog.style.background = 'rgba(255, 255, 255, 0)';
+        this.innerHTML = 'afficher moin';
 
         textContener.affiche = 'true';
     }
@@ -26,5 +28,14 @@ const buttons = document.getElementsByClassName("show-more");
 
 for (let i = 0; i < buttons.length; i++) {
     const button = buttons[i];
-    button.addEventListener("click", showMore);
+    const textButton = button.parentNode;
+    const text = textButton.getElementsByClassName('text')[0];
+    const fog = textButton.getElementsByClassName('fog')[0];
+    
+    if (text.clientHeight > 164) {
+        button.addEventListener("click", showMore);
+    } else {
+        button.style.display = 'none';
+        fog.style.display = 'none';
+    }   
 }
